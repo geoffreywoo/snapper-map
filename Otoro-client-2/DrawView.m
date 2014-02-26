0aa41fa901c4cd38d4540cd79118abb47f0f9d5d
Geoffrey Woo <geoffreywoo@gmail.com>
2014-02-26 00:30:50 -0800

    1.1.2 snappermap

--- Otoro-client-2/DrawView.m
+++ Otoro-client-2/DrawView.m
@@ -14,6 +14,9 @@
     
     NSMutableArray *pathsArray;
     NSMutableArray *currentPath;
+    
+    CGPoint pts[5]; // we now need to keep track of the four points of a Bezier segment and the first control point of the next segment
+    uint ctr;
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder // (1)
@@ -58,6 +61,7 @@
     [self setNeedsDisplay];
 }
 
+
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     NSLog(@"touch began");
@@ -85,6 +89,45 @@
     [pathsArray addObject:[[NSArray alloc] initWithArray:currentPath]];
 }
 
+/*
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"touch began");
+    ctr = 0;
+    UITouch *touch = [touches anyObject];
+    pts[0] = [touch locationInView:self];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint p = [touch locationInView:self];
+    ctr++;
+    pts[ctr] = p;
+    if (ctr == 4)
+    {
+        pts[3] = CGPointMake((pts[2].x + pts[4].x)/2.0, (pts[2].y + pts[4].y)/2.0); // move the endpoint to the middle of the line joining the second control point of the first Bezier segment and the first control point of the second Bezier segment
+        
+        [path moveToPoint:pts[0]];
+        [path addCurveToPoint:pts[3] controlPoint1:pts[1] controlPoint2:pts[2]]; // add a cubic Bezier from pt[0] to pt[3], with control points pt[1] and pt[2]
+        
+        [self setNeedsDisplay];
+        // replace points and get ready to handle the next segment
+        pts[0] = pts[3];
+        pts[1] = pts[4];
+        ctr = 1;
+    }
+    
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+//    [self drawBitmap];
+    [self setNeedsDisplay];
+ //   [path removeAllPoints];
+    ctr = 0;
+}
+ */
 - (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [self touchesEnded:touches withEvent:event];
